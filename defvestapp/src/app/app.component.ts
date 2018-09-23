febe9e0390af4b93f209c485d8ad05f8de02e50e
fab kne <fabian.kneissl@gmx.de>
2018-09-23 18:27:19 +0200

    added random form

--- defvestapp/src/app/app.component.ts
+++ defvestapp/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import PouchDB from 'pouchdb';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- defvestapp/src/app/app.module.ts
+++ defvestapp/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material';
+import {MatInputModule} from '@angular/material';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NoopAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
